2bc4813abcbc359998aec56beb17635c8e09f368
Els Stroobants <elsstroobants@hotmail.com>
2019-07-28 16:31:50 +0200

    make the contents of build available in GitHub so it can be viewed as an app

---
+++ build/precache-manifest.4e8ad5ae6cca3c4aff22886ff5e80f35.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1718aa56c3befd1be4055809f6d2882a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33d64213b4760b7ba8e6",
+    "url": "/static/css/main.c54310fa.chunk.css"
+  },
+  {
+    "revision": "6ab5d61a2997cf2ed804",
+    "url": "/static/js/2.c696e79f.chunk.js"
+  },
+  {
+    "revision": "33d64213b4760b7ba8e6",
+    "url": "/static/js/main.a70a0ee0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "63a5cb52c49c2c23346133b000848988",
+    "url": "/static/media/Monday Bay.63a5cb52.ttf"
+  },
+  {
+    "revision": "91435b96b96779f221eede4e866bdbaa",
+    "url": "/static/media/avocados.91435b96.jpg"
+  },
+  {
+    "revision": "43f9d242e1712500e6db537979b07089",
+    "url": "/static/media/cross_pink.43f9d242.svg"
+  },
+  {
+    "revision": "16e5df6fe41ac3b1be76220dacd64cec",
+    "url": "/static/media/heart_hollow_pink.16e5df6f.svg"
+  },
+  {
+    "revision": "0e04a97c32f2bf601435148a68843f48",
+    "url": "/static/media/heart_solid_white.0e04a97c.svg"
+  },
+  {
+    "revision": "4d89579e534e4ffc0d0c06ff9418de68",
+    "url": "/static/media/lasagna.4d89579e.jpg"
+  },
+  {
+    "revision": "c07793ab6578bcc8a5c985d281ab1d59",
+    "url": "/static/media/searchicon.c07793ab.svg"
+  }
+]);
